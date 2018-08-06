fcc4cc9be14f1d3f7f497c09f163674a5716ece5
watcharapong@vertice-international.com <watcharapong@vertice-international.com>
2018-08-06 19:48:16 +0700

    remove asynccompnent  and  add react-loadable

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ import 'bulma/css/bulma.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import HeaderNavigation from './containers/headernavigation/HeaderNavigation';
-import asyncComponent from './AsyncComponent';
 import NotFound from './components/notfound/NotFound';
+import CircularLoader from './common/components/loader/CircularLoader';
+import Loadable from 'react-loadable';
 
 const AppContainer = styled.div`
   font-family: 'Spirax';
@@ -16,17 +17,23 @@ const ContentContainer = styled.section`
   padding: 0;
 `;
 
-const Home = asyncComponent(() =>
-  import('./containers/homemain/Homemain').then(module => module.default)
-);
+const Home = Loadable({
+  loader: () => import('./containers/homemain/Homemain'),
+  loading: CircularLoader,
+  delay: 300
+});
 
-const Search = asyncComponent(() =>
-  import('./containers/searchmain/Searchmain').then(module => module.default)
-);
+const Search = Loadable({
+  loader: () => import('./containers/searchmain/Searchmain'),
+  loading: CircularLoader,
+  delay: 300
+});
 
-const Movie = asyncComponent(() =>
-  import('./containers/moviedetail/Moivedetail').then(module => module.default)
-);
+const Movie = Loadable({
+  loader: () => import('./containers/moviedetail/Moivedetail'),
+  loading: CircularLoader,
+  delay: 300
+});
 
 class App extends Component {
   render() {
