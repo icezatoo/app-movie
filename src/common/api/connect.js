de25ef1881991c4ab03e05b44126fdaceaba3ae2
ice <watcharapong@vertice-international.com>
2018-08-08 08:51:46 +0700

    Updates

--- src/common/api/connect.js
+++
@@ -1,5 +0,0 @@
-const pathMovie = 'https://api.themoviedb.org/3';
-const api_Key = '545355355a5f3d21e30cf2751d3ac464';
-const pathImage = 'http://image.tmdb.org/t/p/';
-
-export { pathMovie, api_Key, pathImage };
--- src/components/moviedetail/wrappedheader/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as RenderHeaderTitle } from './RenderHeaderTitle';
-export { default as RenderOverview } from './RenderOverview';
-export { default as RenderPercentagecircle } from './RenderPercentagecircle';
-export { default as RenderPlayTrailer } from './RenderPlayTrailer';
-export { default as DialogVideo } from './DialogVideo';
-export { default as RenderGenres } from './RanderGenres';
-export { default as RenderDescript } from './RanderDescript';
